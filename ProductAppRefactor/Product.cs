37324ec5628e08775d3421bcca9f570ba38ea44d
garvita <garvita.pun9@gmail.com>
2025-02-14 23:48:22 +0530

    RefactoredProductApp

---
+++ ProductAppRefactor/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAppRefactor
+{
+    public class Product
+    {
+        private int ProductId { get; set; }
+        private string ProductName { get; set; }
+        private double ProductPrice { get; set; }
+        private double DiscountOnProduct { get; set; }
+        public Product(int productId, string productName, double productPrice, double discountOnProduct)
+        {
+            this.ProductId = productId;
+            this.ProductName = productName;
+            this.ProductPrice = productPrice;
+            this.DiscountOnProduct = discountOnProduct;
+        }
+
+        public double CalculateDiscountedProductPrice()
+        {
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+            Console.WriteLine($"MRP on the product {ProductName} is {ProductPrice}");
+            Console.WriteLine(" ");
+            Console.WriteLine($"Discount on the product {ProductName} is {DiscountOnProduct}%");
+            Console.WriteLine(" ");
+            return ProductPrice - (ProductPrice * (DiscountOnProduct / 100.00));
+        }
+
+        public void DisplayProductDetails()
+        {
+            double storeCalculatedFinalPriceResult = CalculateDiscountedProductPrice();
+            Console.WriteLine("| {0, -19} | {1, -19} | {2, -19} | {3, -19} |",
+                "PRODUCT ID", "PRODUCT NAME", "PRODUCTPRICE", "DISCOUNTED PRICE");
+            Console.WriteLine("| {0, -19} | {1, -19} | {2, -19} | {3, -19} |",
+                ProductId, ProductName, ProductPrice, storeCalculatedFinalPriceResult);
+        }
+    }
+}
---
+++ ProductAppRefactor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAppRefactor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product(121, "Almonds (1 Kg)", 1299.00, 10);
+            Product product2 = new Product(124, "Walnuts (1 Kg)", 1599.00, 15);
+            Console.WriteLine(" ");
+            Console.WriteLine("Welcome To Apna Bazaar");
+            Console.WriteLine(" ");
+            product1.DisplayProductDetails();
+            product2.DisplayProductDetails();
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+        }
+    }
+}
