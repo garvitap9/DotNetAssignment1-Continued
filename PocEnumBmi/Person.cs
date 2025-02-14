cef68ca298e0847bf94d9849337dbde119e3cad4
garvita <garvita.pun9@gmail.com>
2025-02-14 22:15:02 +0530

    enum poc done

---
+++ PocEnumBmi/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocEnumBmi
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string PersonName { get; set; }
+        public int PersonAge { get; set; }
+        public double PersonHeight { get; set; } //Height datatype double
+        public double PersonWeight { get; set; } //Weight in Kgs so datatype double
+
+        public Person(int personId, string personName, int personAge, double personHeight, double personWeight) : this(personId, personName, personAge)
+        {
+            this.PersonHeight = personHeight;
+            this.PersonWeight = personWeight;
+        }
+        public Person(int personId, string personName, int personAge)
+        {
+            this.PersonId = personId;
+            this.PersonName = personName;
+            this.PersonAge = personAge;
+            this.PersonHeight = 5;
+            this.PersonWeight = 50;
+        }
+
+        public double CalculateBMIScore()
+        {
+            double bmiScore = PersonWeight / (PersonHeight * PersonHeight);
+            return bmiScore;
+        }
+
+        public string BodyTypeDeteminer(double bmIScore)
+        {
+
+            if (bmIScore < 18.5)
+            {
+                return Convert.ToString(BodyType.Underweight);
+            }
+            else if (18.5 < bmIScore && bmIScore < 24.9)
+            {
+                return Convert.ToString(BodyType.NormalWeight); 
+            }
+            else if (25.0 < bmIScore && bmIScore < 29.9)
+            {
+                return Convert.ToString(BodyType.OverWeight);
+            }
+            else
+            {
+                return Convert.ToString(BodyType.Obesity);
+            }
+
+        }
+    }
+}
---
+++ PocEnumBmi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocEnumBmi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BMI CALCULATION OF A PERSON USING ENUM With Proof of concept!");
+            Console.WriteLine(" ");
+            Person person1 = new Person(1, "Garima", 16, 1.74, 65.00);
+            Person person2 = new Person(2, "Aakash", 27);
+            Person person3 = new Person(3, "Gagan", 30, 1.70, 100.00);
+            Person person4 = new Person(4, "Deepa", 21, 1.79, 95.00);
+            Program.DisplayPersonDetails(person1);
+            Program.DisplayPersonDetails(person2);
+            Program.DisplayPersonDetails(person3);
+            Program.DisplayPersonDetails(person4);
+        }
+        public static void DisplayPersonDetails(Person person)
+        {
+            Console.WriteLine(" ");
+            double getBmiScore = person.CalculateBMIScore();
+            string getBodyType = person.BodyTypeDeteminer(getBmiScore);
+            Console.WriteLine($"Person Id: {person.PersonId} | Person Name: {person.PersonName} | Person Age: {person.PersonAge} | Person Height: {person.PersonHeight} | Person Weight: {person.PersonWeight}");
+            Console.WriteLine($"Person BMI SCore: {getBmiScore} | Person Body Type: {getBodyType}");
+        }
+    }
+}
