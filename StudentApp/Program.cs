640147061a349ef6ebce9e604ed00094143d7ec6
garvita <garvita.pun9@gmail.com>
2025-02-14 23:38:59 +0530

    Student App added

---
+++ StudentApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(1,"Amar", 9.7);
+            Student student2 = new Student(2,"Reema", 8.5);
+            Student student3 = new Student(3,"Mohini", 10.0);
+            Console.WriteLine("Student Details:");
+            Program.DisplayStudentDetails(student1);
+            Program.DisplayStudentDetails(student2);
+            Program.DisplayStudentDetails(student3);
+        }
+        public static void DisplayStudentDetails(Student student)
+        {
+            Console.WriteLine(" ");
+            
+            Console.WriteLine($"Roll Number: {student.RollNumber}");
+            Console.WriteLine($"Student Name: {student.StudentName}" );
+            Console.WriteLine($"Student CGPA Scored: {student.StudentGpa}");
+            Console.WriteLine($"Student Percentage scored: {student.ConvertCGPAToPercentage()}%");
+
+        }
+    }
+}
---
+++ StudentApp/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StudentApp
+{
+    public class Student
+    {
+        public int RollNumber {  get; set; }
+        public string StudentName {  get; set; }
+        public double StudentGpa {  get; set; }
+
+        public Student(int rollNumber, string studentName, double studentGpa)
+        {
+            //we will check for the conditions on roll number, student name and gpa
+            if (rollNumber <= 0)
+            {
+                Console.WriteLine("The roll number must be positive!");
+            }
+               
+            if (studentName.Length < 5)
+            {
+                studentName = "";
+            }
+                
+
+            if (studentGpa < 1 || studentGpa > 10)
+            { 
+                Console.WriteLine("The CGPA must be between 1 and 10!!");
+            }
+                
+            this.RollNumber = rollNumber;
+            StudentName = studentName;
+            StudentGpa = studentGpa;
+        }
+
+        public double ConvertCGPAToPercentage()
+        {
+            return StudentGpa * 9.5;
+        }
+    }
+}
