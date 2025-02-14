f80427d2195ccfbb3d239336b5f81c271d32191f
garvita <garvita.pun9@gmail.com>
2025-02-14 23:05:52 +0530

    PlayerApp done

---
+++ PlayerApp/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp
+{
+    public class Player
+    {
+        private int PlayerId {  get; set; }
+        private string PlayerName { get; set; }
+        private int PlayerAge { get; set; }
+
+        public Player(int playerId, string playerName, int playerAge) : this(playerId, playerName)
+        { 
+            PlayerAge = playerAge;
+        }
+        public Player(int playerId, string playerName) 
+        {
+            PlayerId = playerId;
+            PlayerName = playerName;
+            PlayerAge = 20; //default age setting
+        }
+
+        public int GetPlayerId()
+        {
+            return this.PlayerId;
+        }
+        public string GetPlayerName()
+        {
+            return this.PlayerName;
+        }
+        public int GetPlayerAge()
+        {
+            return this.PlayerAge; 
+        }
+        public void DisplayPlayerDetails()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine($"Player Id: {GetPlayerId()}");
+            Console.WriteLine($"Player Name: {GetPlayerName()}");
+            Console.WriteLine($"Player Age: {GetPlayerAge()}");
+            Console.WriteLine(" ");
+        }
+        public Player WhoIsElder(Player player)
+        {
+            return (this.PlayerAge > player.PlayerAge) ? this : player;
+        }
+    }
+}
---
+++ PlayerApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player[] players = new Player[5]
+            {
+                new Player(18, "Virat", 35),
+                new Player(10, "Sachin", 30),
+                new Player(7, "Dhoni", 33),
+                new Player(21, "Nehal"),
+                new Player(14, "Bumrah", 27)
+            };
+            Console.WriteLine("Details of the Players: ");
+            foreach (Player player in players)
+            {
+                player.DisplayPlayerDetails();
+            }
+
+            Player eldestPlayer = players[0];
+            for (int i =1; i < players.Length; i++)
+            {
+                eldestPlayer = eldestPlayer.WhoIsElder(players[i]);
+            }
+            Console.WriteLine("The eldest player details are: ");
+            eldestPlayer.DisplayPlayerDetails();
+
+            //Program.DisplayPlayerDetails(player1);
+            //Program.DisplayPlayerDetails(player2);
+            //Program.DisplayPlayerDetails(player3);
+            //Program.DisplayPlayerDetails(player4);
+            //Program.DisplayPlayerDetails(player5);
+
+            //Player eldestGetPlayer = player1.WhoIsElder(player2);
+        }
+
+        
+
+        
+    }
+}
